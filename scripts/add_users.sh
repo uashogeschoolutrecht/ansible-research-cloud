02630296b44cc8a74b2d819db0e52a0afaebe8de
i.alkhoury@anchormen.nl <i_kh@icloud.com>
2021-03-30 13:06:21 +0200

    Remove extra files

--- scripts/add_users.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in `more userlist.txt`
-do
-sudo useradd -m -s /bin/bash $i && sudo echo "$i:$i" | sudo chpasswd
-done
--- scripts/setup_research_drive_for_remote_user.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-ansible -i inventories/"$1.yml" -m script -a "scripts/setup_research_drive_mount.sh $3" --become $2
