8b1f01365d13ae8543a7b84efb1b8f6be5b5476f
rinsrutgers <rins.rutgers@gmail.com>
2022-09-30 16:26:13 +0200

    add totp

--- playbooks/roles/nginx-fastcgi/files/example-script.sh
+++ playbooks/roles/nginx-fastcgi/files/example-script.sh
@@ -5,3 +5,11 @@ echo
 echo  "The date is: $DATA"
 # the variable REMOTE_USER indicates a user authenticated by nginx (if present)
 echo  "Authenticated User: $REMOTE_USER"
+
+service code-server@$REMOTE_USER status | grep 'active (running)' > /dev/null 2>&1
+
+if [ $? != 0 ]
+then
+        sudo systemctl start code-server@$REMOTE_USER > /dev/null
+        echo "Starting code-server" 
+fi
